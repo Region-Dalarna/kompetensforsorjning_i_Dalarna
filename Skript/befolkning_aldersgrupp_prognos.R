ec7f8e7475945b21cae2297890e980e85173eb85
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2023-07-06 12:09:34 +0200

    Uppdatering Jon

--- Skript/befolkning_aldersgrupp_prognos.R
+++ Skript/befolkning_aldersgrupp_prognos.R
@@ -97,64 +97,16 @@ diag_befolkning <-function(region_vekt = "20",
                                          ifelse(between(px_df_folkmangd$ålder,65,79),"65-79 år",
                                                 ifelse(ålder>79,"80+ år",ålder)))))
   
-  if (spara_data==TRUE){
-    openxlsx::write.xlsx(px_df_folkmangd,paste0(output_mapp,filnamn))
-  }
-
-  # # Det första datasettet - innehåller åldersgrupper men inte totalt för alla
-  # # Summerar på år och aldersgrupper
-  # px_df_folkmangd_sum<-px_df_folkmangd %>% 
-  #   group_by(år,aldersgrupper) %>% 
-  #   summarize(`Folkmängd`=sum(`Folkmängd`))
-  # 
-  # # Väljer ut år och filtrerar på dessa
-  # fokus_ar <- c("1968","2000",max(px_df_folkmangd_historik$år),"2031")
-  # px_df_folkmangd_utskrift <- px_df_folkmangd_sum %>% 
-  #   filter(år%in%fokus_ar)
-  # 
-  # # Det andra datasettet, skapar en kategori totalt
-  # px_df_folkmangd_totalt<-px_df_folkmangd_utskrift %>% 
-  #   group_by(år) %>% 
-  #   summarize(`Folkmängd`=sum(`Folkmängd`)) %>% 
-  #   mutate(aldersgrupper="totalt")
-  # 
-  # # Sätter ihop dessa två dataset
-  # px_df_folkmangd_utskrift <- rbind(px_df_folkmangd_utskrift,px_df_folkmangd_totalt)
-  # 
-  # # Använder en faktorvariabel för att styra ordningen på åldersgrupper
-  # px_df_folkmangd_utskrift$aldersgrupper <- factor(px_df_folkmangd_utskrift$aldersgrupper, levels = c("totalt","0-19 år","20-64 år","65-79 år","80+ år"))
-  # 
-
+  # Väljer ut år och filtrerar på dessa
+  #fokus_ar <- c("1968","2000",max(px_df_folkmangd_historik$år),"2031")
   
-  # if(diag_befolkning_aldersgrupper==TRUE){
-  #   diagram_titel <- paste0("Dalarnas befolkning uppdelat på åldersgrupper")
-  #   diagram_typ <- "befolkning_forandring"
-  #   diagramfil <- paste0(diagram_typ, ".png")
-  #   objektnamn <- c(objektnamn,diagram_typ)
-  #   
-  #   gg_obj <- SkapaStapelDiagram(skickad_df = px_df_folkmangd_utskrift %>% 
-  #                                  filter(aldersgrupper!="totalt"), 
-  #                                skickad_x_var = "aldersgrupper", 
-  #                                skickad_y_var = "Folkmängd", 
-  #                                skickad_x_grupp = "år",
-  #                                manual_x_axis_text_vjust=0.7,
-  #                                manual_x_axis_text_hjust=0.3,
-  #                                manual_color = valda_farger,
-  #                                x_axis_lutning = 0,
-  #                                diagram_titel = diagram_titel,
-  #                                diagram_capt = diagram_capt,
-  #                                manual_x_axis_title = "Åldersgrupper",
-  #                                berakna_index = FALSE,
-  #                                output_mapp = output_mapp,
-  #                                filnamn_diagram = diagramfil,
-  #                                skriv_till_diagramfil = skapa_fil)
-  #   
-  #   gg_list[[j]] <-gg_obj
-  #   j=j+1
-  # }
-  # 
-  # names(gg_list)<-c(objektnamn)
-  # return(gg_list)
+  px_df_folkmangd_utskrift <- px_df_folkmangd %>%
+    filter(år%in%c("1968","2000","2010",max(px_df_folkmangd_historik$år),as.character((as.integer(max(px_df_folkmangd_historik$år))+10))))
+  
+  
+  if (spara_data==TRUE){
+    openxlsx::write.xlsx(px_df_folkmangd_utskrift,paste0(output_mapp,filnamn))
+  }
   
 }
 
