bf130caa4dd12b0f6b43845cb05061cddaa0f4e1
Peter Möller <larspetermoller@gmail.com>
2024-10-17 19:33:40 +0200

    Automatisk commit och push av repositoryt till github 17 okt 2024 kl. 19:18

--- Skript/4_push_av_hela_repo_till_github.R
+++ Skript/4_push_av_hela_repo_till_github.R
@@ -7,4 +7,7 @@ source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_AP
 repo_namn <- list.files(here()) %>% .[str_detect(., ".Rproj")] %>% str_remove(".Rproj")
 
 datum_tid <- format(Sys.time(), "%d %b %Y kl. %H:%M")
-github_commit_push(repo = repo_namn, commit_txt = paste0("Automatisk commit och push av repositoryt till github ", datum_tid))
+
+github_commit_push(repo = repo_namn, 
+                   fran_rmarkdown = TRUE,
+                   commit_txt = paste0("Automatisk commit och push av repositoryt till github ", datum_tid))
